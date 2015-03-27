0b8544e03ffbe15b5ff358f017d586a3cf7ee31b
freeman <freeman@freeman-laptop.(none)>
2015-03-28 01:13:00 +0800

    add git alias for update the lastest commit of snipmate-snippets

--- install-vimrc.sh
+++ install-vimrc.sh
@@ -7,3 +7,6 @@ ln -s .vim/gvimrc .gvimrc
 cd .vim/bundle/xmledit/ftplugin/
 ln -s xml.vim html.vim
 ln -s xml.vim xhtml.vim
+
+# git alias
+git config alias.up-sub-snipmate-snippets '!f() { cd bundle/snipmate-snippets && git checkout master && git pull && git submodule update --init --recursive; }; f'
